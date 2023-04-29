e6aa43fc587211339b510f3b39c470d1c5a3868a
adeebatgithub <76395903+adeebatgithub@users.noreply.github.com>
2023-04-29 12:34:34 +0530

    Update settings.py

--- athma_2k23/athma_2k23/settings.py
+++ athma_2k23/athma_2k23/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "adeebdanish063@gmail.com"
-EMAIL_HOST_PASSWORD = "eaewpamhpnstpqbm"
+EMAIL_HOST_USER = "#EMAIL_ID"
+EMAIL_HOST_PASSWORD = "#APP_PASSWORD"
